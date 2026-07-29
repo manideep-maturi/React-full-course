ad5b760f7d5836b61d519fb56a29cf41b1d94422
manideep-maturi <maturimanideep@gmail.com>
2026-07-29 15:47:06 +0530

    code spliting

--- src/App.jsx
+++ src/App.jsx
@@ -325,51 +325,98 @@
 
 //!lazy
 
+// import React from "react";
+// import { lazy, Suspense } from "react";
+// import { Route, Routes } from "react-router-dom";
+// import Navbar from "./React Router/Navbar";
+// import "./navbar.css";
+
+// const Home = lazy(() => import("./React Router/Home"));
+// const About = lazy(() => import("./React Router/About"));
+// const Contact = lazy(() => import("./React Router/Contact"));
+// const Notfound = lazy(() => import("./React Router/Notfound"));
+// const Student = lazy(() => import("./React Router/Student"));
+// const Login = lazy(() => import("./React Router/login"));
+// const Dashboard = lazy(() => import("./React Router/Dashboard"));
+// const Protectedrouter = lazy(() => import("./React Router/Protectedrouter"));
+// const Profile = lazy(() => import("./React Router/Profile"));
+// const Orders = lazy(() => import("./React Router/Orders"));
+// const Services = lazy(() => import("./React Router/Services"));
+
+// const App = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <Suspense fallback={<h1>Loding Avuthundhi Raa.. Wait Chey</h1>}>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/contact" element={<Contact />} />
+//           <Route path="*" element={<Notfound />} />
+
+//           <Route path="/login" element={<Login />} />
+//           <Route
+//             path="/dashboard"
+//             element={
+//               <Protectedrouter>
+//                 <Dashboard />
+//               </Protectedrouter>
+//             }
+//           >
+//             <Route path="profile" element={<Profile />} />
+//             <Route path="services" element={<Services />} />
+//             <Route path="orders" element={<Orders />} />
+//           </Route>
+//           <Route path="/student/:id/:name" element={<Student />}></Route>
+//         </Routes>
+//       </Suspense>
+//     </>
+//   );
+// };
+
+// export default App;
+
+//!Codespliting
+
 import React from "react";
-import { lazy, Suspense } from "react";
+import Home from "./React Router/Home";
+import About from "./React Router/About";
+import Contact from "./React Router/Contact";
+import Notfound from "./React Router/Notfound";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./React Router/Navbar";
 import "./navbar.css";
-
-const Home = lazy(() => import("./React Router/Home"));
-const About = lazy(() => import("./React Router/About"));
-const Contact = lazy(() => import("./React Router/Contact"));
-const Notfound = lazy(() => import("./React Router/Notfound"));
-const Student = lazy(() => import("./React Router/Student"));
-const Login = lazy(() => import("./React Router/login"));
-const Dashboard = lazy(() => import("./React Router/Dashboard"));
-const Protectedrouter = lazy(() => import("./React Router/Protectedrouter"));
-const Profile = lazy(() => import("./React Router/Profile"));
-const Orders = lazy(() => import("./React Router/Orders"));
-const Services = lazy(() => import("./React Router/Services"));
-
+import Student from "./React Router/Student";
+import Login from "./React Router/Login";
+import Dashboard from "./React Router/Dashboard";
+import Protectedrouter from "./React Router/Protectedrouter";
+import Profile from "./React Router/Profile";
+import Orders from "./React Router/Orders";
+import Services from "./React Router/Services";
 const App = () => {
   return (
     <>
       <Navbar />
-      <Suspense fallback={<h1>Loding Avuthundhi Raa.. Wait Chey</h1>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Notfound />} />
-
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/dashboard"
-            element={
-              <Protectedrouter>
-                <Dashboard />
-              </Protectedrouter>
-            }
-          >
-            <Route path="profile" element={<Profile />} />
-            <Route path="services" element={<Services />} />
-            <Route path="orders" element={<Orders />} />
-          </Route>
-          <Route path="/student/:id/:name" element={<Student />}></Route>
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Notfound />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Protectedrouter>
+              <Dashboard />
+            </Protectedrouter>
+          }
+        >
+          <Route path="profile" element={<Profile />} />
+          <Route path="services" element={<Services />} />
+          <Route path="orders" element={<Orders />} />
+        </Route>
+        <Route path="/student/:id/:name" element={<Student />}></Route>
+      </Routes>
     </>
   );
 };
